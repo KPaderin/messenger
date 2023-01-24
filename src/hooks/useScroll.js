d520c910e75a6f8e83b18da1b148aa42ca83aef8
Kirill <kirjaes@yandex.ru>
2023-01-24 12:41:25 +0500

    add scroll hook

---
+++ src/hooks/useScroll.js
@@ -0,0 +1,24 @@
+import {useEffect, useRef} from "react";
+
+const useScroll = (parentRef, childRef, callback) => {
+    const observer = useRef();
+
+    useEffect(() => {
+        const options = {
+            root: parentRef.current,
+            rootMargin: '0px',
+            threshold: 0
+        }
+        observer.current = new IntersectionObserver(([target]) => {
+            if (target.isIntersecting)
+                callback()
+        }, options)
+        childRef.current && observer.current.observe(childRef.current)
+
+        return function () {
+            observer.current.unobserve(childRef.current)
+        };
+    }, [callback, childRef, parentRef])
+};
+
+export default useScroll;
